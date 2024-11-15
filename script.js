44a5aa2b239186d7d24647bb93ba3294f4bce8f1
Tristan Salinas <salinas.tristan@gmail.com>
2024-11-15 10:11:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+addEventListener("DOMContentLoaded", function () {
+  let burgerButtonDOM = document.querySelector(".burger-button");
+  let hiddenMenuDOM = document.querySelector(".footer__hiddenMenu");
+
+  burgerButtonDOM.addEventListener("click", function () {
+    // Basculer la classe "show" sur l'élément du menu caché
+    hiddenMenuDOM.classList.toggle("show");
+  });
+});
